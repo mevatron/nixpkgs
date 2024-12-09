56eceba4f027ddc0220bec1db7cd2c6e0a77ed98
Will Lucas <mevatron@gmail.com>
2024-12-08 18:39:21 -0600

    pkgs/by-name/bk: add bk7231tools

--- pkgs/by-name/bk/bk7231tools/package.nix
+++ pkgs/by-name/bk/bk7231tools/package.nix
@@ -4,32 +4,6 @@
   python3,
 }:
 
-let
-  pyDatastruct = python3.pkgs.buildPythonPackage rec {
-    pname = "py-datastruct";
-    version = "1.1.0"; # Latest appears to be 2.0.0, but bk7231tools depends on 1.x
-    pyproject = true;
-
-    src = fetchFromGitHub {
-      owner = "kuba2k2";
-      repo = "datastruct";
-      rev = "refs/tags/v${version}";
-      hash = "sha256-KEIvibGnQnIDMpmodWN2Az/ypc37ZyGvgVPC7voFmlA=";
-    };
-
-    build-system = with python3.pkgs; [ poetry-core ];
-
-    pythonImportsCheck = [ "datastruct" ];
-
-    meta = with lib; {
-      description = "Combination of struct and dataclasses for easy parsing of binary formats";
-      homepage = "https://github.com/kuba2k2/datastruct";
-      license = licenses.mit;
-      maintainers = with maintainers; [ mevatron ];
-    };
-  };
-in
-
 python3.pkgs.buildPythonApplication rec {
   pname = "bk7231tools";
   version = "2.0.2";
@@ -53,16 +27,16 @@ python3.pkgs.buildPythonApplication rec {
   dependencies = with python3.pkgs; [
     pyserial
     pycryptodome
-    pyDatastruct
+    py-datastruct
   ];
 
   pythonImportsCheck = [ "bk7231tools" ];
 
-  meta = with lib; {
+  meta = {
     description = "Tools to interact with and analyze artifacts for BK7231 MCUs";
     homepage = "https://github.com/tuya-cloudcutter/bk7231tools";
-    license = licenses.mit;
-    maintainers = with maintainers; [ mevatron ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ mevatron ];
     mainProgram = "bk7231tools";
   };
 }
